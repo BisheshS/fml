64a4ebfeb4fa515f68a421c654825231bc1800da
Ancientae0N <slipfold9@gmail.com>
2018-02-26 01:08:47 +0530

    new tests for extended_string.h

--- test.c
+++ test.c
@@ -3,6 +3,11 @@
 #include "traverse.h"
 int main(){
 	char *s = "this/is/a/test";
-	char **p = traverse_down(s);
-	printf("%s\n", p[3]);
+	char **p = path_split(s);
+	int i;
+	for(i = 0; i<path_length(s); i++)
+		printf("%s\n", p[i]);
+	char *test = path_join(p);
+	printf("%s\n", test);
+	
 }
